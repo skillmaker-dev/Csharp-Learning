fa318c35c1f8fa0cb3f91ab4c97e14ce6e29eabd
Anas CHAHID <anas.chahid777@gmail.com>
2021-11-21 16:45:04 +0100

    StackOverflow Exercise

---
+++ Learning/Post.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Learning
+{
+    class Post
+    {
+        public string Title { get; set; }
+        public string Description { get; set; }
+        private int _vote; 
+        public int Vote
+        {
+            get
+            {
+                return this._vote;
+            }
+        }
+        public DateTime Date { get; }
+
+        public Post()
+        {
+            this.Date = DateTime.Now;
+            this.Title = null;
+            this.Description = null;
+            this._vote = 0;
+        }
+
+        public void UpVote()
+        {
+            this._vote++;
+        }
+
+        public void DownVote()
+        {
+            this._vote--;
+        }
+    }
+}
--- Learning/Program.cs
+++ Learning/Program.cs
@@ -8,22 +8,23 @@ namespace Learning
     {
         static void Main(string[] args)
         {
-            var sw = new Stopwatch();
-            sw.Start();
-            Console.WriteLine("stopwatch started");
-            sw.Stop();
-            Console.WriteLine("stopwatch stopped");
-            sw.Stop();
-            Console.WriteLine("stopwatch stopped");
-            
+            var post = new Post();
+            post.Title = "I get an error when i add this line";
+            post.Description = "Lorem ipsum dolor sit amet, " +
+                "consectetur adipiscing elit. Etiam erat massa, " +
+                "dictum non nulla ut, faucibus vulputate nisl." +
+                " Vivamus tempor augue orci, at bibendum metus elementum ac.";
 
-                Console.ReadLine();
-            //sw.Start();
 
-            
+            post.UpVote();
 
-            Console.WriteLine("Duration is {0}",sw.Duration);
-            
+            post.DownVote();
+            post.DownVote();
+            post.DownVote();
+            post.DownVote();
+
+            Console.WriteLine(post.Vote);
+
         }
 
         
