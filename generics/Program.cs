4a82df7888505d04fa1129f51d6cb48ee588f05a
Anas CHAHID <anas.chahid777@gmail.com>
2021-12-02 12:45:18 +0100

    Created Generic function to compare two numbers

---
+++ generics/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace generics
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Utilities<double>.Max(-4.2, -55));
+        }
+    }
+}
---
+++ generics/Utilities.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace generics
+{
+    class Utilities<T> where T : IComparable
+    {
+        
+        public Utilities()
+        {
+            
+        }
+
+        public static T Max(T a,T b)
+        {
+            return (a.CompareTo(b) < 0) ? b : a;
+        }
+    }
+}
