4d996532c45a575b64b64327e7820b7504ab083c
Anas CHAHID <anas.chahid777@gmail.com>
2021-12-06 21:34:44 +0100

    Created extension method

---
+++ Extension Methods/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Extension_Methods
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string text = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.";
+            Console.WriteLine(text.Shorten(10));
+        }
+    }
+}
---
+++ Extension Methods/StringExtensions.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Extension_Methods
+{
+    public static class StringExtensions
+    {
+        public static string Shorten(this string str,int numOfWords)
+        {
+            if (numOfWords < 0)
+                throw new ArgumentOutOfRangeException("Number of words should be >= 0");
+
+            var splitted = str.Split(' ');
+            if (numOfWords >= splitted.Length)
+                return str;
+
+            return string.Join(" ",splitted.Take(numOfWords)) + "...";
+        }
+    }
+}
