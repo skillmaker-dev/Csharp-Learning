5f17d5e118933c8698e8b4aa4e20d42ed9bbf9c2
Anas CHAHID <anas.chahid777@gmail.com>
2021-12-12 00:51:31 +0100

    Created Async/Await example

---
+++ AsyncAwait/Form1.Designer.cs
@@ -0,0 +1,75 @@
+﻿
+namespace AsyncAwait
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Htmlbox = new System.Windows.Forms.TextBox();
+            this.Download = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // Htmlbox
+            // 
+            this.Htmlbox.Location = new System.Drawing.Point(12, 12);
+            this.Htmlbox.Multiline = true;
+            this.Htmlbox.Name = "Htmlbox";
+            this.Htmlbox.Size = new System.Drawing.Size(776, 561);
+            this.Htmlbox.TabIndex = 0;
+            this.Htmlbox.TextChanged += new System.EventHandler(this.Htmlbox_TextChanged);
+            // 
+            // Download
+            // 
+            this.Download.Location = new System.Drawing.Point(343, 579);
+            this.Download.Name = "Download";
+            this.Download.Size = new System.Drawing.Size(89, 23);
+            this.Download.TabIndex = 1;
+            this.Download.Text = "Download";
+            this.Download.UseVisualStyleBackColor = true;
+            this.Download.Click += new System.EventHandler(this.Download_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 614);
+            this.Controls.Add(this.Download);
+            this.Controls.Add(this.Htmlbox);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox Htmlbox;
+        private System.Windows.Forms.Button Download;
+    }
+}
+
---
+++ AsyncAwait/Form1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AsyncAwait
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Htmlbox_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private async void Download_Click(object sender, EventArgs e)
+        {
+            var html = await DownloadHtmlAsync("https://cloudconvert.com/");
+            Htmlbox.Text = html;
+        }
+
+        public async Task<string> DownloadHtmlAsync(string url)
+        {
+            var webclient = new WebClient();
+            return await webclient.DownloadStringTaskAsync(url);
+        }
+    }
+}
